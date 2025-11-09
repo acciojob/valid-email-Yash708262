ea4e3ee578d4d96285178ee3ae42c1400e85d8aa
Yash708262 <yash708262@gmail.com>
2025-11-09 11:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	 if (!str || typeof str !== "string") return false;
+
+  // Regular expression for validating email
+  const regex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+
+  return regex.test(str);
 }
 
 // Do not change the code below.
